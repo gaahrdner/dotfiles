088ece9b446fc7a14a5918f965b88ffa7c8d044d
Philip Gardner <github@philipgardner.com>
2024-06-19 18:13:57 -0500

    Revert "re-arrange some files"
    This reverts commit 8d908fb720f6552cea8cc2458924427b8d313bd9.

--- flake.nix
+++ flake.nix
@@ -4,36 +4,80 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-darwin = {
-      url = "github:LnL7/nix-darwin";
-      inputs.nixpkgs.follows = "nixpkgs";
+        url = "github:LnL7/nix-darwin";
+        inputs.nixpkgs.follows = "nixpkgs";
     };
     home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    brew-nix = {
-      url = "github:BatteredBunny/brew-nix";
-      inputs.brew-api.follows = "brew-api";
-    };
-    brew-api = {
-      url = "github:BatteredBunny/brew-api";
-      flake = false;
+        url = "github:nix-community/home-manager";
+        inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager, brew-nix, ... }:
+  outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager }:
   let
-    system = "aarch64-darwin";
-    pkgs = nixpkgs.legacyPackages.${system};
-  in {
-    homeConfigurations.gaahrdner = home-manager.lib.homeManagerConfiguration {
-      inherit pkgs
+    configuration = {pkgs, ... }: {
 
+        services.nix-daemon.enable = true;
+        # Necessary for using flakes on this system.
+        nix.settings.experimental-features = "nix-command flakes";
+
+        system.configurationRevision = self.rev or self.dirtyRev or null;
+
+        # Used for backwards compatibility. please read the changelog
+        # before changing: `darwin-rebuild changelog`.
+        system.stateVersion = 4;
+
+        # The platform the configuration will be used on.
+        # If you're on an Intel system, replace with "x86_64-darwin"
+        nixpkgs.hostPlatform = "aarch64-darwin";
+
+	security.pam.enableSudoTouchIdAuth = true;
+
+        # Declare the user that will be running `nix-darwin`.
+        users.users.gaahrdner = {
+            name = "gaahrdner";
+            home = "/Users/gaahrdner";
+        };
+
+        # Create /etc/zshrc that loads the nix-darwin environment.
+        programs.zsh.enable = true;
+
+        environment.systemPackages = [ pkgs.neofetch pkgs.vim ];
+
+	homebrew = {
+	  enable = true;
+	  
+	  taps = [];
+	  brews = ["cowsay"];
+	  casks = [];
+	};
+     };
+     homeconfig = { pkgs, ...}: {
+            # this is internal compatibility configuration 
+            # for home-manager, don't change this!
+            home.stateVersion = "23.05";
+            # Let home-manager install and manage itself.
+            programs.home-manager.enable = true;
+
+            home.packages = with pkgs; [];
+
+            home.sessionVariables = {
+                EDITOR = "vim";
+            };
+	    home.file.".vimrc".source = ./.vimrc;
+     };
+  in
+  {
+    darwinConfigurations."maxbeep" = nix-darwin.lib.darwinSystem {
       modules = [
-        ./home.nix
+         configuration
+         home-manager.darwinModules.home-manager  {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.verbose = true;
+            home-manager.users.gaahrdner = homeconfig;
+         }
       ];
     };
-    
-    defaultPackage.${system} = home-manager.defaultPackage.${system};
   };
 }
